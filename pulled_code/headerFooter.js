24d8d6bcd77eb2638038be9e77a5119603774d02
cadmaster1267 <quadraticking@gmail.com>
2026-04-24 10:44:16 -0700

    Big Changes
    Copied GillCD website and modified colors and logos. Added Listings Page

---
+++ pulled_code/headerFooter.js
@@ -0,0 +1,41 @@
+const header = document.querySelector('#header');
+
+window.addEventListener('load', function () {
+    header.innerHTML = `
+    <div class="center_logo">
+            <img src="Photos/Logos/Bonafide Brokerage Logo Light Grey.png" alt="Bonafide Brokerage" class="logo">
+        </div>
+
+        <ul class="menu_bar">
+            <li class="li_menu"><a href="index.html" class="menu_text">Home</a></li>
+            <li class="li_menu"><a href="listings.html" class="menu_text">Listings</a></li>
+            <!-- <li class="li_menu"><a href="listings.html" class="menu_text">Cus</a></li>
+            <li class="li_menu"><a href="remodel_repair.html" class="menu_text">Remodel & Repair</a></li>
+            -->
+            <li class="li_menu"><a href="contact.html" class="menu_text">Contact</a></li>
+
+        </ul>`;
+
+    footer.innerHTML = `
+        <br>
+
+    <div>
+        <img src="Photos/Logos/Bonafide Brokerage Logo Black.png" alt="Bonafide Brokerage" class="footer_logo">
+
+        <ul class="footer_list">
+            <li class="footer_li"><a href="index.html" class="footer_links">Home</a></li>
+            <li class="footer_li"><a href="listings.html" class="footer_links">Listings</a></li>
+            <!-- <li class="footer_li"><a href=" custom_home.html" class="footer_links">Custom Homes</a></li>
+            <li class="footer_li"><a href=" remodel_repair.html" class="footer_links">Remodel & Repair</a></li>
+            -->
+            <li class="footer_li"><a href=" contact.html" class="footer_links">Contact</a></li>
+        </ul>
+    </div>
+    <br>
+    <p class="footer_footnotes">
+        All projects are completed under the General Contracting License held by Two Step LLC - License # 0095074
+    </p>
+    <br>
+    
+    `
+});
